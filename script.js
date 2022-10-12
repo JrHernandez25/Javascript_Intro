50d2fdc01317637327476b9bd84e291fd0235a24
Jr Hernandez <jrhernande25@g.crookcountyschools.org>
2022-10-12 14:36:52 -0700

    added functions to the intro

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("hello world");
+
+let animal = 'cat' ;
+
+console.log(animal);
+
+
+
+var number1 = 5;
+
+var number2 = 10;
+
+function dividing(number1, number2) {
+    let equalToo = number1 / number2
+    console.log("When dividing " + number1 + " divide " + number2 + " is equal to " + equalToo )
+}
+
+function multiplication(number1, number2) {
+    let equalToo = number1 * number2
+    console.log("When multiplying " + number1 + " multiply " + number2 + " is equal to " + equalToo )
+}
+
+function addition(number1, number2){
+    let equalToo = number1 + number2
+    console.log("When adding " + number1 + " plus " + number2 + " is equal to " + equalToo)
+}
+
+function subtraction(number1, number2){
+    let equalToo = number1 - number2
+    console.log("When subtracting " + number1 + " subtract " + number2 + " is equal to " + equalToo)
+}
+
+
+let sum = number1 + number2;
+let diff = number1 - number2;
+let mult = number1 * number2;
+let div = number2 / number1;
+
+dividing(7, 4)
+multiplication(10, 5)
+addition(10, 8)
+subtraction(2, 5)
